b1b2027ae0b4ab1f4c600931e2013f61436b8ce3
User <user@example.com>
2026-07-19 14:16:29 +0530

    Remove Preloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/Hero";
 import CurrentlyLiked from "@/components/CurrentlyLiked";
 import Moods from "@/components/Moods";
-import Preloader from "@/components/Preloader";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Preloader />
       <Hero />
       <CurrentlyLiked />
       <Moods />
